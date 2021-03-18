550ffccd5dfec3496a10fcf9877f0493abca0d06
Murad <muradheyderov@gmail.com>
2021-03-18 21:14:13 +0400

    job will remove old backup files

--- dumpDatabase.sh
+++ dumpDatabase.sh
@@ -1,4 +1,13 @@
+#!/bin/sh
 DUMP_FILE_NAME="backupOn`date +%Y-%m-%d-%H-%M`.dump"
+BACKUP_LIMIT=$BACKUP_LIMIT
+if [ "$BACKUP_LIMIT" == "" ]; then
+  echo "BACKUP_LIMIT not setted. Default BACKUP_LIMIT is 10"
+  BACKUP_LIMIT=10
+else
+  echo "BACKUP_LIMIT setted to $BACKUP_LIMIT"
+fi
+
 echo "Creating dump: $DUMP_FILE_NAME"
 
 cd pg_backup
@@ -9,6 +18,14 @@ if [ $? -ne 0 ]; then
   rm $DUMP_FILE_NAME
   echo "Back up not created, check db connection settings"
   exit 1
+fi
+
+FILES_TO_REMOVE=$(ls -t | sed -e "1,$BACKUP_LIMIT d")
+
+if [ ! -z "$FILES_TO_REMOVE" -a "$FILES_TO_REMOVE" != " " ]; then
+  ls -t | sed -e "1,$BACKUP_LIMIT d" | xargs rm -r 
+
+  echo "Removed files: $FILES_TO_REMOVE"
 fi
 
 echo 'Successfully Backed Up'
